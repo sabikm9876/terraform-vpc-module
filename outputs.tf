692a22a2d6a0795923cf2ce2270e300543b2b643
Maribell Sabik <sabikm9876@outlook.com>
2018-08-08 08:39:20 -0700

    Implement module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "name" {
    value       = "${random_pet.animal.id}"
    description = "Contains the name of a random animal."
 }
+
+output "animal-tmp"
+  value = "${var.prefix}-${random_pet.animal.id}"
+
